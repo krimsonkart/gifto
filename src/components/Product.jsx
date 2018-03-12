d74d6dcbeb8c97cc7261e90c2656f6273259a4a4
Arun Vemireddy <arun.vemireddy@myntra.com>
2018-03-13 00:40:25 +0530

    npm install

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AutoFitImage from 'react-image-autofit-frame';
 
 const Product = (props) => {
   const { url, title, description, image} = props;
@@ -6,7 +7,7 @@ const Product = (props) => {
   return (
     <div className="col-md-3">
       <div className="embed-responsive embed-responsive-16by9">
-        <img className="uk-thumbnail uk-thumbnail-mini uk-align-left" src={image} />
+        <AutoFitImage frameWidth="200px" frameHeight="200px" imgSrc={image} className="uk-thumbnail uk-thumbnail-mini uk-align-left"/>
       </div>
       <div className="video-info">
         <h4><a href="#">{title}</a></h4>
